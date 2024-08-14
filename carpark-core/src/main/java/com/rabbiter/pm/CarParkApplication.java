47ce08b49b44abe831168778fd615024f5d79984
杨明 <798187056@qq.com>
2024-08-14 16:21:39 +0800

    first

---
+++ carpark-core/src/main/java/com/rabbiter/pm/CarParkApplication.java
@@ -0,0 +1,12 @@
+package com.rabbiter.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarParkApplication.class,args);
+    }
+}
